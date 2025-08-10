a9c40e23e8ed434dc25ba08eeb48cc72c4ecdd82
wagehzaietr <bad1game1@hotmail>
2025-08-11 02:29:45 +0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import FooterSection from "@/components/footer";
 import dynamic from "next/dynamic";
 
 const HeroSection = dynamic(() => import("@/components/hero-section"), {
-  ssr: false,
+
 });
 
 export default function Home() {
